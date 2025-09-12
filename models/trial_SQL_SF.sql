0e7492061bc68f76784bfd2f6f1255afafd60a8d
Aishwarya M <samaish1507@gmail.com>
2025-09-12 20:30:22 +0000

    no duplicates

--- models/trial_SQL_SF.sql
+++ models/trial_SQL_SF.sql
@@ -1,9 +1,17 @@
-{{ config(materialized='incremental') }}
+{{ config(materialized='incremental',
+   unique_key = 'ID',
+   incremental_strategy='merge') }}
 
-SELECT
+SELECT distinct
     id,
     UPPER(name) AS customer_name,
     UPPER(email) AS email,
-    TO_VARCHAR(created_at, 'DD-MM-YYYY') AS signup_date
+    TO_DATE(created_at) AS signup_date,
+    CURRENT_TIMESTAMP as updated_at
 FROM {{ source('public', 'customers') }}
 --FROM aish_sql_sf.public.customers;
+
+{% if is_incremental() %}
+    -- only load rows where signedup is newer than the max already in the target table
+    where signup_date > (SELECT COALESCE(MAX(signup_date), '1900-01-01') FROM {{ this }})
+{% endif %}
